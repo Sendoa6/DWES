7c5011caa2555565eb2f97a0034dfb7ac33a57b8
Sendoa6 <sendoa.perez06@somo.eus>
2025-10-17 11:45:26 +0200

    inicio

--- PracticaAlumnado/php/conexiones.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $conexion = mysqli_connect("localhost","root","","alumnos_db");
-    // if ($conexion){
-    //     echo"Conectado exitosamente a la base de datos";
-    // }else{
-    //     echo "No se ha podido conectar a la base de datos";
-    // } 
-?>
